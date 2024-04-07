a06386c0938f1399cf0eb1650c7296d944fac0c9
sᴀɢᴀʀˣᴀᴅɪsᴀ <112709751+stkeditz@users.noreply.github.com>
2024-04-07 17:31:02 +0530

    Update Mmf.py

--- AarohiX/plugins/Management/Mmf.py
+++ AarohiX/plugins/Management/Mmf.py
@@ -52,7 +52,7 @@ async def drawText(image_path, text):
 
     if upper_text:
         for u_text in textwrap.wrap(upper_text, width=15):
-            u_width, u_height = draw.textsize(u_text, font=m_font)
+            u_width, u_height = m_font.getsize(u_text)
 
             draw.text(
                 xy=(((i_width - u_width) / 2) - 2, int((current_h / 640) * i_width)),
@@ -93,7 +93,7 @@ async def drawText(image_path, text):
 
     if lower_text:
         for l_text in textwrap.wrap(lower_text, width=15):
-            u_width, u_height = draw.textsize(l_text, font=m_font)
+            u_width, u_height = m_font.getsize(l_text)
 
             draw.text(
                 xy=(
